555e763c515f9dcba3fb07c291801daec4a4ae9c
Pathetic Geek <geekpathetic@gmail.com>
2020-07-12 22:20:54 +0530

    config updatesss

--- src/config.js
+++ src/config.js
@@ -13,9 +13,47 @@ export default {
       { title: "CONNECT", to: "#connect" }
     ]
   },
-  work: ["/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg"],
-  family: ["/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg"],
-  whosTalking: ["/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg", "/images/logo.jpg"],
+  work: [
+    "/images/showcase/denimstar-1.jpg",
+    "/images/showcase/denimstar-2.jpg",
+    "/images/showcase/denimstar-3.jpg",
+    "/images/showcase/k9-1.jpg",
+    "/images/showcase/punjabispice-1.jpg",
+    "/images/showcase/punjabispice-2.jpg",
+    "/images/showcase/repsfitness-1.jpg",
+    "/images/showcase/repsfitness-2.jpg",
+    "/images/showcase/spacebar-1.jpg",
+    "/images/showcase/spacebar-2.jpg",
+    "/images/showcase/spacebar-3.jpg",
+    "/images/showcase/spacebar-4.jpeg",
+    "/images/showcase/stark-1.jpeg",
+    "/images/showcase/stark-2.jpeg",
+    "/images/showcase/stark-3.jpeg",
+    "/images/showcase/stark-4.jpeg",
+    "/images/showcase/velocity-1.jpg"
+  ],
+  family: [
+    "/images/our-family/asa.png",
+    "/images/our-family/denimstar.jpg",
+    "/images/our-family/k9.jpeg",
+    "/images/our-family/myxibulls.png",
+    "/images/our-family/nutrikhata.jpg",
+    "/images/our-family/punjabispice.png",
+    "/images/our-family/repsfitness.png",
+    "/images/our-family/spacebar.png",
+    "/images/our-family/velocity.jpg"
+  ],
+  whosTalking: [
+    "/images/work-with/fllorence.png",
+    "/images/work-with/image1.jpeg",
+    "/images/work-with/indra.gif",
+    "/images/work-with/patiala.png",
+    "/images/work-with/pune-hip-hop.jpeg",
+    "/images/work-with/saibalaji.jpeg",
+    "/images/work-with/StarCafe.png",
+    "/images/work-with/tedx.png",
+    "/images/work-with/woodpecker.png"
+  ],
   socials: [
     { name: "Facebook", icon: "logo-facebook", link: "", text: "Arc Media" },
     { name: "Phone", icon: "call-outline", link: "", text: "+919467336229" },
@@ -36,85 +74,109 @@ export default {
         "A full time hardworking extrovert with an expertise of 5 years in the marketing, advertising and communication industry; Vivian knows exactly what your business needs and requires at each and every given time of the year. From planning of an event, brainstorming behind each and every aspect of the marketing technique that should be used accurately; behind your business, Vivian knows and covers it all"
     },
     {
-      name: "Yash aryan",
-      img: "https://via.placeholder.com/500?text=Person",
+      name: "Yash Aryan",
+      img: "/images/team/Bunny.jpg",
       position: "Director of photography / D.O.P",
       about:
         "A master of camera handling and a production genius, yash covers it all. With an experience of 3 years in the field of commercial production, conceptualized shoots, wedding and other product shoots; yash makes a mark in the core team of arc media."
     },
     {
-      name: "Sagar surkute",
-      img: "https://via.placeholder.com/500?text=Person",
+      name: "Sagar Surkute",
+      img: "/images/team/SagarSurkute.jpg",
       position: "post production manager",
       about:
         "The head chef of Arc media’s kitchen who takes care of the post production behind every shoot that takes place. Furnishing all the work with his hands dipped in juice of vision and creativity."
     },
     {
-      name: "Vaishnavi jha",
-      img: "https://via.placeholder.com/500?text=Person",
+      name: "Vaishnavi Jha",
+      img: "/images/team/VaishnaviJha.jpg",
       position: "public relations manager and business strategist",
       about:
         "The public relation manager vaishnavi jha makes sure that your business maintains a strong and an excellent public image along with brainstorming various business development strategies for your growth and efficient working. From managing the production, connecting with the clients to delivering of all the client data; she has got it all covered."
     },
     {
       name: "Shivani Tiwari",
-      img: "https://via.placeholder.com/500?text=Person",
+      img: "/images/team/ShivaniTiwari.jpg",
       position: "public relations executive in chief/ business planner",
       about:
         "Shivani is the public relation executive in chief who is hardworking and is responsible for the creative planning of all the clients. From maintaining cordial relations to planning and developing strategies; Shivani does it all."
     },
     {
-      name: "Akanksha singh",
-      img: "https://via.placeholder.com/500?text=Person",
+      name: "Akanksha Singh",
+      img: "/images/team/AkankshaSingh.jpg",
       position: "content writer and social media manager",
       about:
         "All the content that is generated for the clients of arc media are inspected under Akanksha singh. She is also an excellent social media manager who takes control of various digital activities of many clients."
     }
   ],
-  associations: [
-    {
-      name: "The Earth Café",
-      image: "https://via.placeholder.com/500x200",
-      addresses: [
-        "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune",
-        "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune"
-      ],
-      deal: "Get a 15% Discount on the Food Bill at The Earth Cafe.",
-      tandc: ["Valid from Monday to Thursday", "Applicable on a-la-carte dine-in only"]
-    },
-    {
-      name: "The Earth Café",
-      image: "https://via.placeholder.com/500x200",
-      addresses: [
-        "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune",
-        "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune"
-      ],
-      deal: "Get a 15% Discount on the Food Bill at The Earth Cafe.",
-      tandc: ["Valid from Monday to Thursday", "Applicable on a-la-carte dine-in only"]
-    }
-  ],
+  // associations: [
+  //   {
+  //     name: "The Earth Café",
+  //     image: "https://via.placeholder.com/500x200",
+  //     addresses: [
+  //       "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune",
+  //       "Restaurant 1, Upper Ground Floor, Lane 7, Power Plaza, Koregaon Park, Pune"
+  //     ],
+  //     deal: "Get a 15% Discount on the Food Bill at The Earth Cafe.",
+  //     tandc: ["Valid from Monday to Thursday", "Applicable on a-la-carte dine-in only"]
+  //   },
+  // ],
   testimonials: [
     {
-      name: "Rachel Goenka",
-      image: "https://via.placeholder.com/500?text=Person",
-      company: "The Chocolate Spoon Company",
+      name: "",
+      image: "/images/our-family/repsfitness.png",
+      company: "Reps Fitness",
       review:
-        "We've been working with Pomelo Social for over 2 years now and they've become a part of The Chocolate Spoon family! The dedication and commitment that Sasha Anand and his team have towards our brands is commendable and we've done some superb work together. Their approach has always been fresh and out of the box and the energy they bring to the table is exactly what we need from an agency. Keep up the good work and here's looking forward to many more years of great teamwork!"
+        "I m very happy with the work that they provide. They have a great team with enthusiastic people always running about, getting work done. The package that they provide is quite different, which is why i picked them in the first place."
     },
     {
-      name: "Rachel Goenka",
-      image: "https://via.placeholder.com/300?text=Person",
-      company: "The Chocolate Spoon Company",
+      name: "",
+      image: "/images/our-family/punjabispice.png",
+      company: "Punjabi Spice",
       review:
-        "We've been working with Pomelo Social for over 2 years now and they've become a part of The Chocolate Spoon family! The dedication and commitment that Sasha Anand and his team have towards our brands is commendable and we've done some superb work together. Their approach has always been fresh and out of the box and the energy they bring to the table is exactly what we need from an agency. Keep up the good work and here's looking forward to many more years of great teamwork!"
+        "They have an amazing team. The services that they provide, right from the creatives to the cinematic videos are just fabulous. I am completely satisfied with the work the give. My social media account look perfect."
     },
     {
-      name: "Rachel Goenka",
-      image: "https://via.placeholder.com/250?text=Person",
-      company: "The Chocolate Spoon Company",
+      name: "",
+      image: "/images/our-family/spacebar.png",
+      company: "Space Bar",
       review:
-        "We've been working with Pomelo Social for over 2 years now and they've become a part of The Chocolate Spoon family! The dedication and commitment that Sasha Anand and his team have towards our brands is commendable and we've done some superb work together. Their approach has always been fresh and out of the box and the energy they bring to the table is exactly what we need from an agency. Keep up the good work and here's looking forward to many more years of great teamwork!"
+        "I love working with energetic, hardworking people, and the team of Arc media is definitely like that. The representation of my business is done really well. They always come up with great ideas, which i love. I m happy with their work. Plus their service to the clients for all the doubts are very quick and prompt which makes it easier to work along."
+    },
+    {
+      name: "",
+      image: "/images/our-family/k9.jpeg",
+      company: "K9",
+      review:
+        "I have been running a seafood restaurant in Pimple Saudagar for a while now. Arc media not just promotes the USP of our bussiness but makes everything equally valuable. I could have never imagined the interiors of the restaurant being portrayed so beautifully on social media platforms,but Arc media made it possible. They have been a support system for us to do better . Arc media make us reach to people's heart.We look forward to this."
+    },
+    {
+      name: "",
+      image: "/images/work-with/StarCafe.png",
+      company: "Stark cafe",
+      review:
+        "Arc media has helped us make our superhero themed cafe an exclusive one in town. Every superhero needs a break, a concept by the team helped us create our presence not only on social media platforms but also in people's heart. It has been a beautiful journey with Arc media from the very start."
+    },
+    {
+      name: "",
+      image: "/images/our-family/denimstar.jpg",
+      company: "Denim Star",
+      review:
+        "Arc Media have always been able to meet our needs . They are truely excellent partners . They send us things before we even know that we need them. They've cared about how well we do since day 1. I've experienced nothing but complete professionalism from Arc Media."
+    },
+    {
+      name: "",
+      image: "/images/our-family/velocity.jpg",
+      company: "Velocity",
+      review:
+        "Arc media has really amazed us every step of the way. They provide a tremendous amount of quality content. They're flexible with scheduling. The team provides a clear road map for what they want to accomplish amd how they plan to do it . We appreciate their accountability. We're so grateful to have a marketing firm working with us, not for us."
     }
+    // {
+    //   name: "",
+    //   image: "/images/our-family/",
+    //   company: "",
+    //   review: ""
+    // }
   ]
 };
 

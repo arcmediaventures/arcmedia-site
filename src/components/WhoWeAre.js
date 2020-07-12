42b7c17adcb263929f8b7c0627e064f1a0c79259
Pathetic Geek <geekpathetic@gmail.com>
2020-07-13 00:44:28 +0530

    title added

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function WhoWeAre() {
   return (
-    <section id="what-we-do" style={{ backgroundColor: "#eee", padding: 0, backgroundColor: "#FFDE59" }}>
+    <section id="what-we-do" style={{ padding: 0, paddingTop: 36, backgroundColor: "#FFDE59" }}>
+      <h1 className="section-title">What we do</h1>
       <img
         src="/images/whatwedo.jpg"
         style={{ maxHeight: "70vh", width: "100%", objectFit: "contain", objectPosition: "center", maxWidth: 1024 }}

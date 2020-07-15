63d7b695bb2aba707e29f9f31731291583d78228
Pathetic Geek <geekpathetic@gmail.com>
2020-07-15 23:06:16 +0530

    address added

--- src/components/SocializeWithUs.js
+++ src/components/SocializeWithUs.js
@@ -20,6 +20,22 @@ export default function SocializeWithUs({ socials }) {
           </li>
         ))}
       </ul>
+      <h1 className="section-title" style={{ marginTop: 24 }}>
+        Visit our headoffice
+      </h1>
+      <div className="address">
+        <img src="/images/girl_walk.png" alt="girl walking" />
+        <div className="text">
+          Arc media advertising and marketing office, Service road ground floor next to Denim Star, Laxmi-Narayan Nagar, Wadmukhwadi,
+          Charholi Budruk, Pimpri-Chinchwad, Maharashtra 412105
+        </div>
+        <a
+          href="https://www.google.com/maps?q=Laxmi-Narayan+Nagar,+Wadmukhwadi,+Charholi+Budruk,+Pimpri-Chinchwad,+Maharashtra+412105"
+          aria-label="map"
+        >
+          <img src="/images/map.png" alt="map" />
+        </a>
+      </div>
     </section>
   );
 }
